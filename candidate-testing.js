1007074168f7a81ae0435e3ca3d5625c7ca841ec
dlm <55953676+pizzadancer@users.noreply.github.com>
2021-06-27 18:34:46 +0000

    Removed numbering from example in order to get autograder to work

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,11 +10,11 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "1) Who was the first American woman in space? ",
-  "2) True or false: 5 kilometer == 5000 meters? ",
-  "3) (5 + 3)/2 * 10 = ? ",
-  "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "5) What is the minimum crew size for the ISS? "
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
   "Sally Ride",
